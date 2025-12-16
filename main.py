94bc59d96b433139f7537b48bdc98bf9607d7879
hkbridges <hastenkbridges@gmail.com>
2025-12-16 17:51:49 -0600

    circleshape.py

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
 
+    clock = pygame.time.Clock()
+    dT = 0
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -20,6 +23,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dT = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
