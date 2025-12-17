c5ee8ab969580d56db328a0abfdf086df9c5f3bc
hkbridges <hastenkbridges@gmail.com>
2025-12-16 18:07:29 -0600

    player rotation

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     pygame.init()
 
     clock = pygame.time.Clock()
-    dT = 0
+    dt = 0
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
@@ -23,9 +23,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dT = clock.tick(60)/1000
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
